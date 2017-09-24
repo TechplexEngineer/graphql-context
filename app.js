beca75db1f0cb4984278b2bbe749a86e928f284b
Blake Bourque <Techplex.Engineer@gmail.com>
2017-09-24 08:12:40 -0400

    Example of using context to pass a new dataloader for each request

--- app.js
+++ app.js
@@ -8,10 +8,45 @@ app.get('/', function(req, res) {
   res.redirect('/graphql');
 });
 
-app.use('/graphql', graphqlHTTP({
+const rp = require('request-promise');
+const _ = require('lodash');
+const DataLoader = require('dataloader');
+
+const BASE_URL = 'https://swapi.co/api'; //no trailing /
+// Use dataloader to batch URL requests for each layer of the query
+const restLoader = new DataLoader((urls) => {
+	console.log("load", urls);
+	return Promise.all(urls.map((url) => {
+		return rp({
+			uri: url,
+			json: true,
+		});
+	}));
+});
+
+
+function fetchResponseByURL(relativeURL, elem) {
+	console.log(`${BASE_URL}${relativeURL}`);
+	return restLoader.load(`${BASE_URL}${relativeURL}`).then((res) => {
+		if (elem) {
+			return _.get(res, elem);
+		}
+		return res;
+	});
+}
+
+app.get('/graphql', graphqlHTTP({
 	schema: Schema,
-	graphiql: true
+	graphiql: true,
 }));
+
+app.post('/graphql', (req, res) => {
+	let ret = graphqlHTTP({
+		schema: Schema,
+		context: fetchResponseByURL
+	})
+	return ret(req, res);
+});
 
 app.listen(3500);
 
--- schema.js
+++ schema.js
@@ -7,33 +7,6 @@ const {
 	GraphQLInt,
 } = require('graphql');
 
-const rp = require('request-promise');
-const _ = require('lodash');
-const DataLoader = require('dataloader');
-const BASE_URL = 'https://swapi.co/api'; //no trailing /
-
-// Use dataloader to batch URL requests for each layer of the query
-const restLoader = new DataLoader((urls) => {
-	console.log("load", urls);
-	return Promise.all(urls.map((url) => {
-		return rp({
-			uri: url,
-			json: true,
-		});
-	}));
-});
-
-function fetchResponseByURL(relativeURL, elem) {
-	console.log(`${BASE_URL}${relativeURL}`);
-	return restLoader.load(`${BASE_URL}${relativeURL}`).then((res) => {
-		if (elem) {
-			return _.get(res, elem);
-		}
-		return res;
-	});
-}
-
-
 const PersonType = new GraphQLObjectType({
 	name: 'VirtualMachine',
 	description: 'Somebody that you used to know',
@@ -48,7 +21,7 @@ const PersonType = new GraphQLObjectType({
 		},
 		mass: {
 			type: GraphQLString,
-			resolve: (root, args) => root.mass,
+			resolve: (root, args, ctx) => root.mass,
 		}
 	}),
 });
@@ -60,7 +33,9 @@ const QueryType = new GraphQLObjectType({
 
 		people: {
 			type: new GraphQLList(PersonType),
-			resolve:  (root, args) => fetchResponseByURL(`/people`, 'results')
+			resolve:  (root, args, fetch) => {
+				return fetch(`/people`, 'results');
+			}
 		}
 	}),
 });
